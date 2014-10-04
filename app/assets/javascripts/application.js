aa2607f300aa5ef2598b29ad22096e7876d933a1
Ahmed <ahmed1490@gmail.com>
2014-10-04 18:16:07 +0530

    removed database dependency from rails app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// require jquery
+//= require jquery
 // require jquery_ujs
 // require turbolinks
 // require_tree .
 
-//= require_tree ./libs
+//= require_tree ./lib
 //= require ./controllers/controllers.js
 //= require ./services/services.js
 //= require ./directives/directives.js
